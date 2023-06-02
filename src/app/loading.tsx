3883c633a488b579837d49b8da3d6bd179a248ec
saufth <saufth@gmail.com>
2023-06-02 14:47:31 -0600

    Loading span ajustment & Button style refactored

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ import { THEMES } from '@/modules/theme/config'
 /** The main loading component of application */
 export default function Loading () {
   return (
-    <div className='w-screen h-screen px-12 fixed top-0 left-0 z-80 overflow-hidden grid place-content-center gap-y-2 bg-gradient-to-r from-primary-dark to-primary-light'>
+    <div className='w-screen h-screen px-12 fixed top-0 left-0 z-80 overflow-hidden grid place-content-center gap-y-1 bg-gradient-to-r from-primary-dark to-primary-light'>
       <div className='max-w-xs'>
         <Logo
           alt='Loading...'
@@ -14,7 +14,7 @@ export default function Loading () {
           priority
         />
       </div>
-      <span className='font-primary-light text-lg text-white text-right'>
+      <span className='font-primary-light text-white text-right'>
         Cargando...
       </span>
     </div>
